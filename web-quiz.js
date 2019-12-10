02fb1637be546a42a066502d1b18905c817b0d93
Chris Aragon <aragonchristopherray@gmail.com>
2019-12-10 12:26:18 -0500

    latest update it's jumping to one instead of starting at 0

---
+++ web-quiz.js
@@ -0,0 +1,123 @@
+'use strict';
+const STORE = {
+    questions: [
+    //question 1
+    {options: ['Ford', 'General Motors', 'Cheverolet', 'Cadillac'], answer: 'Cheverolet'},
+    //question 2
+    {options: ['Lexus', 'Infiniti', 'Honda', 'Mitsubishi'], answer: 'Infiniti'},
+    //question 3
+    {options: ['Ferrari', 'Porche', 'Lamborghini', 'Jaguar'], answer: 'Ferrari'},
+    //question 4
+    {options: ['Lexus', 'Suburu', 'Lotus', 'Lincoln'], answer: 'Lexus'},
+    //question 5
+    {options: ['BMW', 'Nissan', 'Audi', 'Marcedes-Benz'], answer: 'Marcedes-Benz'},
+    //question 6
+    {options: ['Mazda', 'Toyota', 'Ford', 'General Motors'], answer: 'Toyota'},
+    //question 7
+    {options: ['BMW', 'Rolls Royce', 'Volkswagen', 'Bently'], answer: 'Volkswagen'},
+    //question 8
+    {options: ['Cadillac', 'Hummer', 'Jeep', 'Bucker'], answer: 'Cadillac'},
+    //question 9
+    {options: ['Bently', 'Acura', 'Audi', 'Mazda'], answer: 'Audi'},
+    //question 10
+    {options: ['Mini', 'Subaru', 'Ford', 'Chrysler'], answer: 'Subaru'},
+    //question 11
+    {options: ['Saturn', 'Fiat', 'Tatra', 'Tesla'], answer: 'Tesla'},
+    //question 12
+    {options: ['Maserati', 'Saturn', 'Lambougini', 'Mercury'], answer: 'Maserati'}
+    ],
+    currentQuestion: 0,
+    totalScore: 0
+}
+
+const logo = ["../fotos/Chevrolet-logo.jpg", '../fotos/infiniti-logo.jpg', '../fotos/Ferrari-Logo.jpg',
+'../fotos/Lexus-Logo.jpg', '../fotos/toyota.jpg', '../fotos/volkswagen.logo.jpg', '../fotos/cadillac-logo.jpg',
+'../fotos/audi-logo.jpg', '../fotos/subaru-logo.jpg', '../fotos/tesla-logo.jpg', '../fotos/maserati-logo.jpg']
+
+
+
+//function to track the current question and increase the score if the correct answer is selected
+function increaseScore(){
+    STORE.currentQuestion++
+}
+
+
+
+function beginTheLogoQuiz(){
+    /* when this button is activated, the <h1>, <h2>, paragraph tags, and button should disappear
+    replaced with the content of the quiz (i.e. either removed or hidden). It should also be replaced
+    with the first question of the form, including the <h1>Which logo is this</h1>, the logo image,
+    and the four buttons to represent the options */
+    $(".start_button").click(function(event){
+        $("header").remove();
+        $(".logo_question").toggle();
+        $(".image_and_options").prepend(answerPrompt())
+    })
+    console.log("`beginTheLogoQuiz` has executed")
+
+}
+function answerPrompt(){
+    $(".image").append(
+        `<div>
+        <img src="${logo[STORE.currentQuestion]}">
+        </div`
+    )
+    $(".question_options").append(
+        `<div>
+        <input type=radio>${STORE.questions[STORE.currentQuestion].options[0]}</input>
+        <input type=radio>${STORE.questions[STORE.currentQuestion].options[1]}</input>
+        <input type=radio>${STORE.questions[STORE.currentQuestion].options[2]}</input>
+        <input type=radio>${STORE.questions[STORE.currentQuestion].options[3]}</input>
+        <div>`
+    )
+}
+
+
+function answer(){
+    /* when this button is hit, the answer is highlighted in a green border, and the next button appears
+    to go on to the next question. The score and question number will both update by 1 */
+    console.log("`Answer` has executed")
+    if ($(".button").val() === STORE.questions[STORE.currentQuestion].answer){
+        $(this).append(
+            `<div>
+            <p>very good! you got the right answer!</p>
+            </div`
+        )
+    } else {
+        `<div>
+            <p>oh no! you were wrong! the correct answer was ${STORE.questions[STORE.currentQuestion].answer.val()}</p>
+        </div`
+    }
+}
+
+function theNextButton(){
+    /* when this button is hit, the next question will appear. Specifically, the title will stay the same, but the 
+    logo as well as the four possible questions will update. The "next" button will dissapear again until the next
+    answer is selected */
+    console.log("`theNextButton` has executed")
+
+}
+
+function showMyResults(){
+    /* at the end of the 12th question, the page will update and show one of 4 pages based on the score. 
+    A score of between 0 and 3 will show the messed-up car. A score of between a 4 and a 7 will show an old
+    minivan. A score of between 7-10 will show a nice gray car. A score of either 11 or 12 will show the 
+    red sports car. There will also appear a personalized message as well as a button to restart the quiz*/
+    console.log("`showMyResults` has executed")
+}
+
+function retryQuiz(){
+    /* this button will be at the bottom of all of the final scores, and it will reshow the home page and will 
+    restart the score at zero. It will allow the user to flip through the quiz all over again */
+    console.log("`retryQuiz` has executed")
+}
+
+function thingyTest(){
+    increaseScore();
+    beginTheLogoQuiz();
+    answer();
+    theNextButton();
+    showMyResults();
+    retryQuiz();
+}
+$(thingyTest);
