54ff3c9883a2ec1a5c59c8425ae0c431913bdd17
Chris Aragon <aragonchristopherray@gmail.com>
2019-12-16 21:16:47 -0500

    gettin' that darn reset button

--- web-quiz.js
+++ web-quiz.js
@@ -55,8 +55,8 @@ function beginTheLogoQuiz(){
         $("header").remove();
         $(".logo_question").toggle();
         $(".image_and_options").prepend(answerPrompt())
-        $(".question_counter").html(STORE.currentQuestion + 1 + " / 12")
-        $(".score_counter").html(STORE.totalScore + " / 12")
+        $(".question_counter").html(STORE.currentQuestion + 1 + " / " + STORE.questions.length)
+        $(".score_counter").html(STORE.totalScore + " / " + STORE.questions.length)
     })
     console.log("`beginTheLogoQuiz` has executed")
 }
@@ -90,7 +90,7 @@ function submitAnswer(){
                     </div>`
                 ), increaseScore();
                 theNextButton();
-                $(".score_counter").html(STORE.totalScore + " / 12")
+                $(".score_counter").html(STORE.totalScore + " / " + STORE.questions.length)
             } else if (!$("input[name='test1']:checked").val()) {
                 alert('Please choose an option')
              }
@@ -109,8 +109,13 @@ function theNextButton(){
     logo as well as the four possible questions will update. The "next" button will dissapear again until the next
     answer is selected */
     console.log("`theNextButton` has executed")
-    $(".submit_answer").replaceWith(`<button class="next_question">Next Question!</button>`)
-    loadNextQuestion();
+    if (STORE.currentQuestion < 11){
+        $(".submit_answer").replaceWith(`<button class="next_question">Next Question!</button>`)
+        loadNextQuestion();
+        } else {
+            $(".submit_answer").replaceWith(`<button class="final_question">Show My Results!</button>`)
+            showMyResults();
+    }
 }
 
 function loadNextQuestion(){
@@ -121,28 +126,85 @@ function loadNextQuestion(){
         $(this).remove();
         increaseNumber();
         answerPrompt();
-      })
+        $(".question_counter").html(STORE.currentQuestion + 1 + " / " + STORE.questions.length)
+    })
 }
 
 function showMyResults(){
     /* at the end of the 12th question, the page will update and show one of 4 pages based on the score. 
     A score of between 0 and 3 will show the messed-up car. A score of between a 4 and a 7 will show an old
-    minivan. A score of between 7-10 will show a nice gray car. A score of either 11 or 12 will show the 
+    minivan. A score of between 8-11 will show a nice gray car. A score of 12 will show the 
     red sports car. There will also appear a personalized message as well as a button to restart the quiz*/
-    console.log("`showMyResults` has executed")
-    
+    $(".final_question").click(function(event){
+        $(".image_and_options").remove()
+        $(".logo_question").replaceWith(
+            `<h2>You got ${STORE.totalScore} / ${STORE.questions.length} car logos!`)
+        if (STORE.totalScore >= 11){
+            $(".results").append(
+                `<div>
+                    <img src="../fotos/yellow-sports-car.jpg">
+                </div>
+                <div>
+                    <p>Incredible! You knew every logo! Outstanding job!</p>
+                </div>
+                
+                <button class="reset">Click to take another lap!</button>`
+            )
+        } else if (STORE.totalScore >= 8){
+            $(".results").append(
+                `<div>
+                    <img src="../fotos/not-too-shabby-car.jpg">
+                </div>
+                <div>
+                    <p>Pretty good, pretty good. Not perfect but still pretty good</p>
+                </div>
+                
+                <button class="reset">Click to take another lap!</button>`
+            )
+        } else if (STORE.totalScore >=4){
+            $(".results").append(
+                `<div>
+                    <img src="../fotos/old-minivan.jpg">
+                </div>
+                <div>
+                    <p>That was....mehhh. You're fine....not great, just....fine</p>
+                </div>
+                
+                <button class="reset">Click to take another lap!</button>`
+            )
+        } else {
+            $(".results").append(
+            `<div>
+                    <img src="../fotos/clunker.jpg">
+                </div>
+                <div>
+                    <p>That was awful! You should study up on your car logos</p>
+                </div>
+                
+                <button class="reset">Click to take another lap!</button>`
+            )
+        }
+ })
 }
 
 function retryQuiz(){
     /* this button will be at the bottom of all of the final scores, and it will reshow the home page and will 
     restart the score at zero. It will allow the user to flip through the quiz all over again */
     console.log("`retryQuiz` has executed")
+    $(".reset").click(function(event){
+        alert("peter these are parachusetes")
+        /*STORE.currentQuestion = 0
+        STORE.totalScore = 0
+        $(".question_counter").html(STORE.currentQuestion + 1 + " / " + STORE.questions.length)
+        $(".score_counter").html(STORE.totalScore + " / " + STORE.questions.length)
+        $(".results").remove()
+        $(".image_and_options").prepend(answerPrompt()) */
+    })
 }
 
 function thingyTest(){
     beginTheLogoQuiz();
     theNextButton();
-    showMyResults();
     retryQuiz();
     submitAnswer();
 }
